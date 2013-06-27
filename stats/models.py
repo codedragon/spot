2198f6df3871bf2b6c10eddea61d1c28ccceadf9
Maria McKinley <mariak@mariakathryn.net>
2013-06-27 14:03:40 -0700

    updated settings, urls, enabled django admin

--- stats/models.py
+++ stats/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import ModelForm
 
 class Owner(models.Model):
     first_name = models.CharField(max_length=255,
@@ -6,7 +7,7 @@ class Owner(models.Model):
     last_name = models.CharField(max_length=255,
                                  )
     def __unicode__(self):
-        return self.last_name
+        return self.last_name, self.first_name
 
 class Dog(models.Model):
     dog_name = models.CharField(max_length=255,
@@ -16,6 +17,13 @@ class Dog(models.Model):
     def __unicode__(self):
         return self.dog_name
 
+class OwnerForm(ModelForm):
+    class Meta:
+        model = Owner
+
+class DogForm(ModelForm):
+    class Meta:
+        model = Dog
 
 
 
